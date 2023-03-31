baba0080e3064f5fc7ef4893ca695fbb5c32e7c8
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-31 03:24:30 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0;
+
+
+for (let element of batteryBatches){
+    totalBatteries = totalBatteries + element;
+}
+
